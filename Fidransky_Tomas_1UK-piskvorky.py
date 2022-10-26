21215160175e5bc61f69c7499ae5808094b67b26
TFidransky <tom.fidransky@gmail.com>
2022-10-26 22:44:42 +0200

    UDP_ukoly

--- Fidransky_Tomas_1UK-piskvorky.py
+++ Fidransky_Tomas_1UK-piskvorky.py
@@ -1,7 +1,7 @@
 from math import sqrt
-from turtle import forward, right, left, exitonclick, speed, penup, pendown, circle
+from turtle import forward, right, left, exitonclick, setpos, speed, penup, pendown, circle
 
-speed(9)
+speed(10)
 
 #velikost hracího pole
 a = int(input("Zadejte šířku hracího pole (=množství sloupců): "))
@@ -25,39 +25,26 @@ right(90)
 forward(50*b)
 left(90)
 
-while True: 
+while True: #samotná hra, střídání mezi hráčem 1 a 2, počet kol není omezen, ani pozice, lze dát [4,4] při hraní na 3x3 poli
     c = int(input("Hráč 1, zadejte x-ovou souřadnici: "))
     d = int(input("Hráč 1, nyní zadejte y-ovou souřadnici: "))
-    forward((50*c)-50)
-    left(90)
-    forward((50*d)-50)
-    right(90)
-    #křížek z [0,0]
+    penup()
+    setpos(((50*c)-50),((50*d)-50))
+    pendown()
     left(45)
     forward((sqrt(5000)))
     right(135)
     forward(50)
     right(135)
     forward((sqrt(5000)))
-    #vrácení na [0,0]
-    left(135)
-    forward(50*d)
-    right(90)
-    forward((50*c)-50)
-    right(180)
     c = int(input("Hráč 2, nyní zadejte X-ovou souřadnici: "))
     d = int(input("Hráč 2, nyní zadejte y-ovou souřadnici: "))
-    #kolečko z [0,0]
-    forward((50*c)-50)
-    left(90)
-    forward((50*d)-50)
-    right(90)
+    penup()
+    setpos(((50*c)-50),((50*d)-50))
+    right(135)
     forward(25)
+    pendown()
     circle(25)
-    left(180)
-    forward(25+(50*c)-50)
-    left(90)
-    forward((50*d)-50)
-    left(90)
+    
 
 exitonclick()
