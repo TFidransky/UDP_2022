648e864fd5deb92d0688ae0b49eb3cab931d5c90
TFidransky <tom.fidransky@gmail.com>
2022-10-28 23:10:57 +0200

    UDP_ukoly

--- Fidransky_Tomas_1DU-piskvorky.py
+++ Fidransky_Tomas_1DU-piskvorky.py
@@ -5,9 +5,16 @@ from unicodedata import digit
 speed(0)
 
 #velikost hraciho pole - volitelna velikost
-stranaX = int(float(input("Zadejte šířku hracího pole (=množství sloupců): ")))
-stranaY = int(float(input("Zadejte výšku hracího pole (=množství řádků): ")))
+stranaX = input("Zadejte šířku hracího pole (=množství sloupců): ")
+if not stranaX.isdigit():
+    stranaX = print ("Nesprávný input, šířka hracího pole musí být ve formátu celých čísel")
 
+stranaY = input("Zadejte šířku hracího pole (=množství sloupců): ")
+if not stranaY.isdigit():
+    stranaY = print ("Nesprávný input, šířka hracího pole musí být ve formátu celých čísel")
+  
+stranaX = int(stranaX)
+stranaY = int(stranaY)
 
 #vykresleni hraciho pole - ctverec
 for y in range(stranaY):
@@ -23,9 +30,8 @@ for y in range(stranaY):
     right(90)
 
 for i in range(int((stranaX*stranaY)/2)): #samotna hra, stridani mezi hracem 1 (zde) a 2; na tomto řádku omezení počtu kol, závisí na velikosti hracího pole - jedná se o maximální možný počet tahů
-    while True: #hráč 1, zadá souřadnici x, ověří se, že je OK, poté Y a stejné ověření, pokud OK, nakreslí na vybraném místě křížek
-        hraX = input("Hráč 1, zadejte x-ovou souřadnici: ")
-        
+    while True: #hráč 1, zadá souřadnici "x", ověří se, že je OK, poté "y" a stejné ověření, pokud OK, nakreslí na vybraném místě křížek
+        hraX = input("Hráč 1, zadejte x-ovou souřadnici: ")        
         if not hraX.isdigit() or int(hraX) > stranaX: 
             hraX = print("Nesprávný input, x-ová souřadnice by měla být ve formátu celého čísla a mělo by být v intervalu od 1 do",stranaX,".")
         else:
@@ -52,7 +58,7 @@ for i in range(int((stranaX*stranaY)/2)): #samotna hra, stridani mezi hracem 1 (
     right(135)
     forward((sqrt(5000)))
 
-    #HRAC 2 - opět zadá X a Y souřadnice, if zjišťuje podmínky, pokud jsou  OK, tak na vybraném políčku vykreslí kolečko, poté volí místo hráč 1 a to se opakuje do nekonečna
+    #HRAC 2 - opět zadá "x" a "y" souřadnice, if zjišťuje podmínky, pokud jsou  OK, tak na vybraném políčku vykreslí kolečko, poté volí místo hráč 1 a to se opakuje do nekonečna
     while True:    
         hraX = input("Hráč 2, zadejte x-ovou souřadnici: ")
         
